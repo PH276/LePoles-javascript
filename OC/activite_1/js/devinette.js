37a99fa0a6edb33b39bcfe3e6a66df713097c502
PH276 <pascal.huitorel@lepoles.com>
2017-07-18 02:06:50 +0200

    OC1 acrtivite1 (fin)

--- OC/activite_1/js/devinette.js
+++ OC/activite_1/js/devinette.js
@@ -9,7 +9,6 @@ console.log("Bienvenue dans ce jeu de devinette !");
 
 // Cette ligne génère aléatoirement un nombre entre 1 et 100
 var solution = Math.floor(Math.random() * 100) + 1;
-console.log(solution);
 // Décommentez temporairement cette ligne pour mieux vérifier le programme
 //console.log("(La solution est " + solution + ")");
 
