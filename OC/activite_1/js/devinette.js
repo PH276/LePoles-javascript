45631cd891a823e56ed8cb7998c8c53c197d3fce
PH276 <pascal.huitorel@lepoles.com>
2017-07-18 02:02:06 +0200

    OC1 activite 1

--- OC/activite_1/js/devinette.js
+++ OC/activite_1/js/devinette.js
@@ -10,7 +10,6 @@ console.log("Bienvenue dans ce jeu de devinette !");
 // Cette ligne génère aléatoirement un nombre entre 1 et 100
 var solution = Math.floor(Math.random() * 100) + 1;
 console.log(solution);
-
 // Décommentez temporairement cette ligne pour mieux vérifier le programme
 //console.log("(La solution est " + solution + ")");
 
@@ -18,7 +17,7 @@ console.log(solution);
 var essai = 0;
 var nombreEssai=0;
 const NOMBREESSAIMAX = 6;
-while (essai != solution && nombreEssai < 6){
+while (essai != solution && nombreEssai < NOMBREESSAIMAX) {
     nombreEssai++;
     essai = prompt ("Entrer un nombre entre 1 et 100 :");
     if (essai < solution){
@@ -28,10 +27,9 @@ while (essai != solution && nombreEssai < 6){
     }
 }
 
-if (nombreEssai < 6) {
+if (essai == solution && nombreEssai <= NOMBREESSAIMAX) {
     console.log('Bravo ! la solution était ' + solution);
     console.log('Vous avez trouvé en '+ nombreEssai + ' essai(s)');
-}
-else {
+} else {
     console.log('Perdu... la solution était ' + solution);
 }
